6f18193daef29e5ca00585dc60040bdb0bfd75e4
Grind-Master01 <sanyaolusamuel009@gmail.com>
2026-05-30 20:03:22 +0100

    Samuel Initial

--- note.js
+++ note.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT ||4000;
 
 
 let notes = [
-  { id : crypto.randomBytes(4).readUInt32BE(0), title: "Backend Course", author: "John Dee", description: "Learn backend development with Node.js and Express.js " },
-  { id : crypto.randomBytes(4).readUInt32BE(0), title: "Backend Course", author: "John Leo", description: "Learn backend development with Node.js and Express.js " },
+  { id : crypto.randomBytes(4).readUInt32BE(0), title: " I'd Rather Be Writing ", author: "Tom Johnson", description: " Tom Johnson is a senior technical writer at Google and a widely recognized authority in the technical communications field. He is best known for his blog and extensive API Documentation Course hosted on I'd Rather Be Writing, which serves as a definitive resource for writers transitioning into developer documentation. The I'd Rather Be Writing course is a comprehensive, self-paced guide designed to help technical writers understand the tools and methodologies required for high level developer documentation." },
+  { id : crypto.randomBytes(4).readUInt32BE(0), title: "Twilio's Developer Education team", author: "John Leo", description: "Twilio's Developer Education team provides curated open-source notes for beginners through the TwilioDevEd/introduction-to-apis-notes repository on GitHub. These notes accompany a comprehensive course designed to break down the fundamentals of Application Programming Interfaces (APIs). The course is structured into units that guide you from basic concepts to building functional applications." },
   { id : crypto.randomBytes(4).readUInt32BE(0), title: "Backend Course", author: "John bay", description: "Learn backend development with Node.js and Express.js " },
   { id : crypto.randomBytes(4).readUInt32BE(0), title: "Backend Course", author: "John Gee", description: "Learn backend development with Node.js and Express.js " },
 ];
@@ -39,9 +39,10 @@ app.get('/notes/:id', (req, res, next) =>{
 
 
 
-// app,post('/notes', (req, res) => {
-
-// })
+ app.post ('/notes', (req, res) => {
+  
+  res.status(201).json({ error: "Input invalid"});
+  });
 
 app.use ((err, req, res, next) => {
   res.status(500).json({ error: "Server error!"});
